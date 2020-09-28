1521de87fb0422761306c7d8e1db388ab4cf69e5
Lee Michaeli <leeisr@gmail.com>
2020-09-28 05:23:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(string)
   new = ""
-  string.split.collect do |word|
-    word.reverse
-    new << word
-    new << " "
+  string.split.collect {|word| word.reverse}.join(" ")
   end
   new
 end
